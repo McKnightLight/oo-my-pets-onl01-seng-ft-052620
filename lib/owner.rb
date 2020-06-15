697d8f4f066e8b491821f820a36df6a18392a83b
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-15 23:14:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,8 +4,10 @@ class Owner
   attr_accessor :name
   attr_reader :species 
   
-  def initialize(name)
-    @name = name 
+  def initialize(name, species=("human")) 
+    @name=name 
+    @species=species 
   end
+  
   
 end
